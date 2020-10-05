07fdabce50ba7b5f8776ce1fafc6e025fb618636
TSPruner <56366430+TSPruner@users.noreply.github.com>
2020-10-05 15:04:58 -0400

    Add files via upload

---
+++ orders/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import ChristmasGreen, TreeStand, Bow, Bells, Angel, Star, Bulb, Ribbon, Lights, TableRunner, TreeSkirt, Review, Order
+
+# Register your models here.
+admin.site.register(ChristmasGreen)
+admin.site.register(TreeStand)
+admin.site.register(Bow)
+admin.site.register(Bells)
+admin.site.register(Angel)
+admin.site.register(Star)
+admin.site.register(Bulb)
+admin.site.register(Ribbon)
+admin.site.register(Lights)
+admin.site.register(TableRunner)
+admin.site.register(TreeSkirt)
+admin.site.register(Review)
+admin.site.register(Order)
---
+++ orders/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class OrdersConfig(AppConfig):
+    name = 'orders'
+
+class UsersConfig(AppConfig):
+    name = 'users'
